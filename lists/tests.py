6f6ad8dd351a153527f9bdb3672d7d0bc5ae4347
root <csp5096@yahoo.com>
2018-02-03 21:28:01 -0600

    new URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -17,32 +17,31 @@ class HomePageTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(),0)
 
-    def test_displays_all_list_items(self):
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 class ItemModelText(TestCase):
 
-    def text_saving_and_retrieving_items(self):
+    def test_saving_and_retrieving_items(self):
 
         first_item = Item()
         first_item.text = 'The first (ever) list item'
@@ -61,3 +60,4 @@ class ItemModelText(TestCase):
 
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
