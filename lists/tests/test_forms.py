16ed0d37514ec47216e2452df2a196250d6fc8e3
root <csp5096@yahoo.com>
2018-02-10 19:59:52 -0600

    Advance form constructor

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,21 +1,35 @@
 from django.test import TestCase
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm
+)
 
-class ItemFormTest(TestCase):
+class ExistingListItemFormTest(TestCase):
 
     def test_form_renders_item_text_input(self):
-        form = ItemForm()
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
         self.assertIn('placeholder="Enter a to-do item"', form.as_p())
-        self.assertIn('class="form-control input-lg"', form.as_p())
 
     def test_form_validation_for_blank_items(self):
-        form = ItemForm(data={'text': ''})
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_,
+                                    data={'text': ''})
         self.assertFalse(form.is_valid())
         self.assertEqual(
             form.errors['text'],
             [EMPTY_ITEM_ERROR]
         )
+
+    def test_form_validation_for_duplicat_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text='no twins!')
+        form = ExistingListItemForm(for_list=list_,
+                                    data={'text': 'no twins!'}
+                                    )
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
     def test_form_save_handles_saving_to_a_list(self):
         list_ = List.objects.create()
