3377495b99f9b05ec55317ecc3a677e715201de7
root <csp5096@yahoo.com>
2018-02-22 21:10:28 -0600

    lists can have owners, which are saved on creation.

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,6 +1,9 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from lists.models import Item, List
+
+User = get_user_model()
 
 class ItemModelText(TestCase):
 
@@ -55,3 +58,11 @@ class ListModelTest(TestCase):
     def test_string_representation(self):
         item = Item(text='some text')
         self.assertEqual(str(item), 'some text')
+
+    def test_lists_can_have_owners(self):
+        user = User.objects.create(email='a@b.com')
+        list_ = List.objects.create(owner=user)
+        self.assertIn(list_, user.list_set.all())
+
+    def test_list_owner_is_optional(self):
+        List.objects.create() # should not raise
