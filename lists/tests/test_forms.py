4b5ea594d6502f12d85cf4ed8691e19b273e2870
root <csp5096@yahoo.com>
2018-02-25 19:25:21 -0600

    add list owners via forms. more isolated tests

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -33,14 +33,6 @@ class ExistingListItemFormTest(TestCase):
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
 
-    def test_form_save_handles_saving_to_a_list(self):
-        list_ = List.objects.create()
-        form = ItemForm(data={'text': 'do me'})
-        new_item = form.save(for_list=list_)
-        self.assertEqual(new_item, Item.objects.first())
-        self.assertEqual(new_item.text, 'do me')
-        self.assertEqual(new_item.list, list_)
-
     def test_form_save(self):
         list_ = List.objects.create()
         form = ExistingListItemForm(for_list=list_, data={'text': 'hi'})
@@ -72,3 +64,12 @@ class NewListFormTest(unittest.TestCase):
         mock_List_create_new.assert_called_once_with(
             first_item_text='new item text', owner=user
         )
+
+    @patch('lists.forms.List.create_new')
+    def test_save_returns_new_list_object(self, mock_List_create_new):
+        user = Mock(is_authenticated=True)
+        form = NewListForm(data={'text': 'new item text'})
+        form.is_valid()
+        response = form.save(owner=user)
+        self.assertEqual(response, mock_List_create_new.return_value)
+
