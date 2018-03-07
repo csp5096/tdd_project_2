a9dcd51d14241490de1c07ef62d59de2115bf6b1
root <csp5096@yahoo.com>
2018-03-07 13:36:19 -0600

    Modified API.py for serializers

--- lists/api.py
+++ lists/api.py
@@ -1,18 +1,23 @@
 from django.http import HttpResponse
 from lists.models import List, Item
+from rest_framework import routers, serializers, viewsets
 
-import json
+class ItemSerializer(serializers.ModelSerializer):
 
-def list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    if request.method == 'POST':
-        Item.objects.create(list=list_, text=request.POST['text'])
-        return HttpResponse(status=201)
-    item_dicts = [
-        {'id': item.id, 'text': item.text}
-        for item in list_.item_set.all()
-    ]
-    return HttpResponse(
-        json.dumps(item_dicts),
-        content_type='application/json'
-    )
+    class Meta:
+        model = Item
+        fields = ('id', 'text')
+
+class ListSerializer(serializers.ModelSerializer):
+    items = ItemSerializer(many=True, source='item_set')
+
+    class Meta:
+        model = List
+        fields = ('id', 'items',)
+
+class ListViewSet(viewsets.ModelViewSet):
+    queryset = List.objects.all()
+    serializer_class = ListSerializer
+
+router = routers.SimpleRouter()
+router.register(r'lists', ListViewSet)
