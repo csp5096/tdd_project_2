8b805713cf6d2924f805538959fb16fc7deaead1
root <csp5096@yahoo.com>
2018-03-07 15:47:43 -0600

    Used Configuration to fix duplication error

--- lists/api.py
+++ lists/api.py
@@ -1,15 +1,27 @@
 from django.http import HttpResponse
 from lists.models import List, Item
 from rest_framework import routers, serializers, viewsets
+from lists.forms import DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR
+from rest_framework.validators import UniqueTogetherValidator
 
 class ItemSerializer(serializers.ModelSerializer):
+    text = serializers.CharField(allow_blank=False,
+                                 error_messages = {'blank': EMPTY_ITEM_ERROR}
+                                 )
 
     class Meta:
         model = Item
         fields = ('id', 'list', 'text')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Item.objects.all(),
+                fields=('list', 'text'),
+                message=DUPLICATE_ITEM_ERROR
+            )
+        ]
 
 class ListSerializer(serializers.ModelSerializer):
-    items = ItemSerializer(many=True, source='item_set')
+    items = ItemSerializer(many=True, source='item_set', read_only=True)
 
     class Meta:
         model = List
